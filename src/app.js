580c10d12c813da436c67b1f347e56c567b26237
MayarQasrawi <mayar.ya12@gmail.com>
2025-08-26 01:32:27 +0300

    Main application setup

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDB = require('./config/database');
+const errorHandler = require('./middleware/errorHandler');
+const noteRoutes = require('./routes/notes');
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+// Body parser
+app.use(express.json());
+
+// Routes
+app.use('/api/v1/notes', noteRoutes);
+
+// Error handler (must be last)
+app.use(errorHandler);
+
+module.exports = app;
